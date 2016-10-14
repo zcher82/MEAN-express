76cdd0fb61845805935a7b44af29ee8f2f1cf1db
zeng her <zcher82@gmail.com>
2016-10-14 13:09:42 -0500

    added css

--- app.js
+++ app.js
@@ -4,12 +4,7 @@ var path = require('path');
 
 app.set('port', 3000);
 
-app.get('/', function(req, res) {
-  console.log("GET the homepage");
-  res
-    .status(200)
-    .sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/json', function(req, res) {
   console.log("GET the json");
