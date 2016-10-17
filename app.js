481108fb57405b84c318dd6275475a8c3fc0cd2b
zeng her <zcher82@gmail.com>
2016-10-17 15:21:22 -0500

    additional express content

--- app.js
+++ app.js
@@ -2,23 +2,19 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var routes = require('./routes');
+
 app.set('port', 3000);
+
+//logging middleware usage
+app.use(function(req, res, next) {
+  console.log(req.method, req.url);
+  next();
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/json', function(req, res) {
-  console.log("GET the json");
-  res
-    .status(200)
-    .json({"jsonData" : true});
-});
-
-app.get('/file', function(req, res) {
-  console.log("GET the file");
-  res
-    .status(200)
-    .sendFile(path.join(__dirname, 'app.js'));
-});
+app.use('/api', routes);
 
 var server = app.listen(app.get('port'), function() {
   var port = server.address().port;
